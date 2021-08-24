882744372d559d04493cf3bbbf2fecd186abb488
Vladimir Pak <pac-man@yandex.ru>
2021-08-24 11:00:49 +1000

    task made

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
-class main {
-    fun endings(){
-        
-    }
+fun main(){
+    val likes = 19846561
+    if ((likes-1)%10 == 0) println("Понравилось $likes человеку")
+    else println("Понравилось $likes людям")
 }
